dbd7a796246f6bb4734ec7c774776e368ab2c492
zgbdsg <zgbdsg@gmail.com>
2014-07-11 18:41:11 +0800

    add jpush and change app icon ;

--- src/com/android/famcircle/ui/MainActivity.java
+++ src/com/android/famcircle/ui/MainActivity.java
@@ -4,8 +4,12 @@ import java.io.File;
 import java.lang.reflect.Field;
 
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 //import android.app.Fragment;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,23 +19,37 @@ import android.view.MenuItem;
 //import android.view.View;
 //import android.view.ViewGroup;
 import android.view.ViewConfiguration;
-
+import cn.jpush.android.api.JPushInterface;
 import com.android.famcircle.R;
 import com.famnotes.android.base.AppManager;
 import com.famnotes.android.base.BaseActivity;
 import com.famnotes.android.db.DBUtil;
 import com.famnotes.android.util.ACache;
+import com.famnotes.android.util.ExampleUtil;
 import com.famnotes.android.vo.Groups;
 import com.famnotes.android.vo.User;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class MainActivity extends BaseActivity {
 	
+	public static boolean isForeground = false;
+	//for receive customer msg from jpush server
+	private MessageReceiver mMessageReceiver;
+	public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+	public static final String KEY_TITLE = "title";
+	public static final String KEY_MESSAGE = "message";
+	public static final String KEY_EXTRAS = "extras";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
+		
+        registerMessageReceiver();
+        
 		try {
             ViewConfiguration config = ViewConfiguration.get(this);
             Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
@@ -68,6 +86,52 @@ public class MainActivity extends BaseActivity {
 			dir.mkdirs();
 	}
 
+	
+	@Override
+	protected void onResume() {
+		isForeground = true;
+		super.onResume();
+	}
+
+
+	@Override
+	protected void onPause() {
+		isForeground = false;
+		super.onPause();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		unregisterReceiver(mMessageReceiver);
+		super.onDestroy();
+	}
+	
+	
+	public void registerMessageReceiver() {
+		mMessageReceiver = new MessageReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+		filter.addAction(MESSAGE_RECEIVED_ACTION);
+		registerReceiver(mMessageReceiver, filter);
+	}
+
+	public class MessageReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (MESSAGE_RECEIVED_ACTION.equals(intent.getAction())) {
+              String messge = intent.getStringExtra(KEY_MESSAGE);
+              String extras = intent.getStringExtra(KEY_EXTRAS);
+              StringBuilder showMsg = new StringBuilder();
+              showMsg.append(KEY_MESSAGE + " : " + messge + "\n");
+              if (!ExampleUtil.isEmpty(extras)) {
+            	  showMsg.append(KEY_EXTRAS + " : " + extras + "\n");
+              }
+			}
+		}
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
